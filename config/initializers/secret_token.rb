9e64336b5338ad1ffd30a9730e4271f153a6ec80
Ricardo <chaneque500@gmail.com>
2013-10-07 20:13:32 -0500

    Primera version del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuayolo::Application.config.secret_key_base = '1304bf66b6781e965118a4e61c5df5e83743defeb7b38b3bfdb8dc12106224516f2ccef5693c3f4f24b192a313849257eb631df87204128928d4081add945fca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cuayolo::Application.config.session_store :cookie_store, key: '_Cuayolo_session'
