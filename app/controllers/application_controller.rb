8920010d5d7697ac609a181a668da66b0cd742d5
Ricardo <chaneque500@gmail.com>
2013-10-24 17:51:10 -0500

    Navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
-def not_authenticated
-	redirect_to login_url, :alert => "Primer login de acceso a esta pagina"
-	
-end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  add_flash_types :default, :primary, :success, :info, :warning, :danger
+
+  def not_authenticated
+		redirect_to login_url, :alert => "Primer login de acceso a esta pagina"
+	end
 end
